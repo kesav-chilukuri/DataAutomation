f5e00617a80e765bb2cbad4be1c50baa171ae5b0
kesav-chilukuri <kesav.chilukuri@gmail.com>
2026-02-06 22:24:18 +0530

    test_sample1

--- test_sample.py
+++ test_sample.py
@@ -8,5 +8,4 @@ spark = SparkSession.builder \
       .getOrCreate()
 dataList = [("Java", 20000), ("Python", 100000), ("Scala", 3000)]
 df=spark.createDataFrame(dataList, schema=['Language','fee'])
-
 df.show()
