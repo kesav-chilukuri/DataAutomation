00b431afb9ac7150542e300d18b9b2d22d55b9d9
kesav-chilukuri <kesav.chilukuri@gmail.com>
2026-02-06 22:36:03 +0530

    conftest update

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,9 @@ def spark_session():
     spark=SparkSession.builder.master("local[1]").appName("DataAutomation").getOrCreate()
     return spark
     spark.stop()
+
+@pytest.fixture(scope="module")
+def read_config():
+
+@pytest.fixture(scope="module")
+def read_data(spark_session,read_config):
